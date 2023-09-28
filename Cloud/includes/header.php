770f8ae535622c530025651531b6c23e25784268
Myra Ng <myrangky@gmail.com>
2023-09-28 11:46:09 +0800

    comm

--- Cloud/includes/header.php
+++ Cloud/includes/header.php
@@ -41,7 +41,6 @@
     <!--Navbar -->
     <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
         <img src="committee/includes/logo.png" width="150px" alt=""/>
-        <a class="navbar-brand" href="index.php">TARUMT</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
         aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
