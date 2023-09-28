d160e08c1adad31cb6c6255ea6495983e65dd3be
Kai Xian <ookx-pm20@student.tarc.edu.my>
2023-09-29 04:48:32 +0800

    remove internship portal

--- Cloud/committee/includes/header.php
+++ Cloud/committee/includes/header.php
@@ -46,7 +46,6 @@
     <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
         
       <img src="includes/logo.png" width="150px" alt=""/>
-      <h4 class="pt-2 mr-4" style="font-weight: bold">&nbsp;Internship Portal</h4>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
         aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
