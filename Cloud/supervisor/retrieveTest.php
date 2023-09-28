f748d2d16ab4f5b28fb9e06aa9fbbcd4f6ff1ee6
Myra Ng <myrangky@gmail.com>
2023-09-28 12:03:42 +0800

    comm

---
+++ Cloud/supervisor/retrieveTest.php
@@ -0,0 +1,23 @@
+<?php 
+	session_start();
+	include 'includes/header.php';
+	include '../core/init.php';
+        
+$sql = "SELECT * FROM studentreport ";
+
+$files = $db->query($sql);
+
+while($file = mysqli_fetch_assoc($files)):
+    ?>
+<a href="<?php echo $file['indemnityReport'];?>" target="_blank" download><?php echo $file['indemnityReport'];?>
+    </a>
+<a href="<?php echo $file['companyAccLetter'];?>" target="_blank" download><?php echo $file['companyAccLetter'];?>
+    </a>
+<a href="<?php echo $file['parentAck'];?>" target="_blank" download><?php echo $file['parentAck'];?>
+    </a>
+        
+<?php
+endwhile;
+
+?>
+
